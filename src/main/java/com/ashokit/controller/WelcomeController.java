c75ae5bb8e6e4f39d77ec7fca6c64dea41b3d052
praveen <praveenltmk2011@gmail.com>
2020-11-19 16:29:58 +0530

    cs04: Annotations added to WelcomeController & pom.xml modified

--- src/main/java/com/ashokit/controller/WelcomeController.java
+++ src/main/java/com/ashokit/controller/WelcomeController.java
@@ -1,9 +1,13 @@
 package com.ashokit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class WelcomeController {
 
-
+	@GetMapping("/welcome")
 	public String showWelcome(Model model){
 		model.addAttribute("msg", "Welcome to Ashok IT ...!!!");
 		return "index";
