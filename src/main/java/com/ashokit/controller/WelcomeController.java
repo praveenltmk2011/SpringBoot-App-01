7f3c077a7d004a2c8ac660a07b238396cd3dd4f5
praveen <praveenltmk2011@gmail.com>
2020-11-19 16:22:55 +0530

    cs05:method added to WelcomeController

--- src/main/java/com/ashokit/controller/WelcomeController.java
+++ src/main/java/com/ashokit/controller/WelcomeController.java
@@ -1,6 +1,11 @@
 package com.ashokit.controller;
 
+
 public class WelcomeController {
-	
-	
+
+
+	public String showWelcome(Model model){
+		model.addAttribute("msg", "Welcome to Ashok IT ...!!!");
+		return "index";
+	}
 }
